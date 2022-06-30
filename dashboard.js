444e711fcba389ac3c43e2580c5e54369fa0c342
hellnoid <xainhellnoid22@gmail.com>
2022-06-30 17:42:56 +0530

    workInProgress..

---
+++ dashboard.js
@@ -0,0 +1,26 @@
+var songs=[
+    {url:"img/1.jpg",name:"Bollywood Butter"},
+    {url:"img/2.jpg",name:"Punjabi 101"},
+    {url:"img/3.jpg",name:"Kollywood Cream"},
+    {url:"img/4.jpg",name:"Indie India"},
+    {url:"img/5.jpg",name:"Tollywood Pearls"},
+    {url:"img/6.jpg",name:"India Top 50"},
+    {url:"img/7.jpg",name:"Sleep"},
+    {url:"img/8.jpg",name:"New Music Hindi"},
+    {url:"img/9.jpg",name:"Dinner"}
+]
+
+// var search ="";
+// var arr;
+// if(search.isempty){
+//     arr = songs;
+// }else{
+//     arr = songs.filter(search);
+// }
+
+
+// fuction display(arr){
+//     for item in arr{
+//         display(items)l
+//     }
+// }
