cd916a7e679245869c6e8ec8a21e7aed9839b43e
Benazir sultana <benazirsultana1998@gmail.com>
2022-07-01 16:16:35 +0530

    player complete

--- spotifyloginpage/index.js
+++
@@ -1,6 +0,0 @@
-//document.querySelector("#five").addEventListener("click",login());
-function login(){
-   event.preventDefault()
-    document.location.href = "/dashboard.html"
-    alert("Login successfull")
-}
