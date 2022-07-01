a9419d380dd473be28eacc3baa9b1060d645b7ac
hellnoid <xainhellnoid22@gmail.com>
2022-07-01 09:42:01 +0530

    10722

--- dashboard.js
+++ dashboard.js
@@ -10,6 +10,46 @@ var songs=[
     {url:"img/9.jpg",name:"Dinner"}
 ]
 
+
+var input = document.getElementById("searchbar");
+
+
+input.addEventListener("keypress", function() {
+  
+  // var myDiv = document.getElementsByClassName("songItemContainer");
+  // myDiv.innerHTML = "";
+
+  var arr=[];
+  var inputValue = document.getElementById("searchbar").value;
+  arr = songs.filter(function(eles){
+    return eles.name.includes(inputValue);
+  })
+  console.log(arr);
+
+display(arr);
+})
+
+function display(arr){
+  document.querySelector(".songItemContainer").innerHTML="";
+  for(var i=0;i<arr.length;i++){
+    var divi=document.createElement("div");
+    var im=document.createElement("img");
+    im.setAttribute("src",arr[i].url);
+    var name=document.createElement("p");
+    name.innerText=arr[i].name;
+  
+    divi.append(im,name);
+    document.querySelector(".songItemContainer").append(divi);
+  }
+}
+
+
+
+
+
+
+
+
 // var search ="";
 // var arr;
 // if(search.isempty){
