780b6b5642775bee3e89fb4beb13f08146d8184b
Benazir sultana <benazirsultana1998@gmail.com>
2022-07-01 22:49:02 +0530

    bug fixed

--- spotifysignup/index.js
+++ spotifysignup/index.js
@@ -1,32 +1,33 @@
-document.querySelector("#signup").addEventListener("click",sign);
-function sign(event){
-    
-    
+document.querySelector("#signup").addEventListener("click", sign);
+function sign(event) {
+
+
     event.preventDefault();
-    
-    
-    
+
+
+
+
+    var email = document.querySelector("#email").value;
+    console.log(email)
+    localStorage.setItem("email", email);
+    var confirm = document.querySelector("#confirmemail").value;
+    if (email !== confirm) {
+        var p = document.createElement("p")
+        p.innerText = "The email does not match"
+        p.style.color = "red"
+        document.querySelector("#emailnotsame").append(p)
+    }
+    else {
+        window.location.href = "/dashboard.html"
+        alert("signup successful");
+    }
+
+    var password = document.querySelector("#createpassword").value;
+    localStorage.setItem("password", password)
+    var name = document.querySelector("#accountname").value
+    localStorage.setItem("accountname", name)
+
    
-var email = document.querySelector("#email").value;
-console.log(email)
-localStorage.setItem("email",email);
-var confirm = document.querySelector("#confirmemail").value;
-if(email!==confirm){
- var p = document.createElement("p")
- p.innerText="The email does not match"
- p.style.color="red"
- document.querySelector("#emailnotsame").append(p)
-}
-else{
-    window.location.href="/dashboard.html"
-}
-
-var password = document.querySelector("#createpassword").value;
-localStorage.setItem("password",password)
-var name = document.querySelector("#accountname").value
-localStorage.setItem("accountname",name)
-
-alert("signup successful");
 
 
 }
